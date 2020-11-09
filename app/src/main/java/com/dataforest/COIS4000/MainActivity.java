5c967cd166b4e5502a505ad7a86c48b05f59f204
Kevin Clarke <kevinclarke@trentu.ca>
2020-11-09 11:59:53 -0500

    Test MainActivity push - Kevin

--- app/src/main/java/com/dataforest/COIS4000/MainActivity.java
+++ app/src/main/java/com/dataforest/COIS4000/MainActivity.java
@@ -51,6 +51,8 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
 
+        //testing MainActivity push
+
         return super.onOptionsItemSelected(item);
     }
 }
