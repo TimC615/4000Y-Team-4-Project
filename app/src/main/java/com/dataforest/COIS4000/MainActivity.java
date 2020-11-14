4e6946d1b5a19dc0e5483323957349690cafc7db
Matthew Duff <matthewfduff@gmail.com>
2020-11-14 13:06:31 -0500

    Testing Push
    Alrighty then...

--- app/src/main/java/com/dataforest/COIS4000/MainActivity.java
+++ app/src/main/java/com/dataforest/COIS4000/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Alrighty then...", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
