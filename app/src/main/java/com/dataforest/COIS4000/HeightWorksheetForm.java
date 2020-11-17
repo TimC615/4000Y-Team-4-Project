17be2b612493d891debdd23a63f704c42fc30694
Kevin Clarke <kevinclarke@trentu.ca>
2020-11-17 16:29:13 -0500

    Height Worksheet Form - Kevin

---
+++ app/src/main/java/com/dataforest/COIS4000/HeightWorksheetForm.java
@@ -0,0 +1,26 @@
+package com.dataforest.COIS4000;
+
+public class HeightWorksheetForm extends PlotForm{
+
+    /*Main Canopy(field 5) is a list of present species
+     * for now I'm making this a record
+     * The same is true for Sub Canopy(field 6)*/
+    final int numRecordFieldsCanopy = 1;
+
+    /*field 7*/
+    final int numRecordFieldsSpecies = 6;
+
+    /*Veterans - no field#*/
+    final int getNumRecordFieldsVeterans = 5;
+
+    public HeightWorksheetForm(String fileToRead, int numFields){
+        super(fileToRead, numFields);
+
+        Field[] recordFieldsCanopy = new Field[numRecordFieldsCanopy];
+        Field[] recordFieldsSpecies = new Field[numRecordFieldsSpecies];
+        Field[] recordFieldVeterans = new Field[getNumRecordFieldsVeterans];
+
+        //init records
+
+    }
+}
