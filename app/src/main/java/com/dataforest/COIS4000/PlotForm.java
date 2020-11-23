19126f89caaf522799bb781c94fc1c1d0d70f64b
Kevin Clarke <kevinclarke@trentu.ca>
2020-11-23 00:48:48 -0500

    JSON parsing - Kevin
    started working on PlotForm constructor

--- app/src/main/java/com/dataforest/COIS4000/PlotForm.java
+++ app/src/main/java/com/dataforest/COIS4000/PlotForm.java
@@ -1,5 +1,11 @@
 package com.dataforest.COIS4000;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Iterator;
+
 public abstract class PlotForm {
     int formId;
     int packageId;
@@ -10,6 +16,30 @@ public abstract class PlotForm {
     public PlotForm(String fileToRead, int numFields){
         /*pull field constructor info from external file*/
         fields = new FormAttr[numFields];
+    }
+
+    /*
+    * initializes fields with a JSONObject
+    * takes the output from StaticMethods.JSONAssetToJSONObject()
+    * */
+    private void getFieldsFromJSONObject(JSONObject formObject) throws JSONException {
+
+        //get first object - name of the form
+        formObject = formObject.getJSONObject(formObject.keys().next());
+
+        JSONArray fieldNames = formObject.names();
+
+        JSONObject current;
+
+        fields = new FormAttr[fieldNames.length()];
+
+        //get JSONObject for each field
+        for(int i = 0; i < fieldNames.length(); i++){
+            current = formObject.getJSONObject(fieldNames.getString(i));
+            //check if record
+            //if record, call record constructor on JSONObject
+            //if not record, call field constructor on JSONObject
+        }
     }
 
     public boolean isComplete(){
