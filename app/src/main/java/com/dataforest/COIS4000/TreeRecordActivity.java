7d77a58e86bfa78ba6739bf26c039af5748a70d2
Timothy Chaundy <timothychaundy@trentu.ca>
2020-11-28 14:04:36 -0500

    Record Page UI

--- app/src/main/java/com/dataforest/COIS4000/TreeRecordActivity.java
+++ app/src/main/java/com/dataforest/COIS4000/TreeRecordActivity.java
@@ -2,16 +2,6 @@ package com.dataforest.COIS4000;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.lifecycle.ViewModel;
-import androidx.lifecycle.ViewModelProvider;
-
-import android.text.Layout;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -19,16 +9,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
-import android.widget.Toast;
 
-public class TreeFormActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TreeRecordActivity extends AppCompatActivity {
 
     //hardcoding values for the prototype
     private final String[] fieldNames = {
-            "Plot",
-            "Date",
-            "Growth Plot Number",
-            "Crown Closure %",
             "Section #",
             "Tree #",
             "Tag Type",
@@ -48,25 +35,26 @@ public class TreeFormActivity extends AppCompatActivity {
             "Decay Class",
             "Ocular Length (m)",
             "Mort. Cause",
-            "Broken Top",
-            "Notes"
+            "Broken Top"
     };
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.tree_form);
+        setContentView(R.layout.tree_record);
 
 
         /*
-        * the rest if this method is making fragments with parameters
-        * if we have a different set of fragments for the record window, this all needs to be done again with thoseXS
-        * */
+         * the rest if this method is making fragments with parameters
+         * if we have a different set of fragments for the record window, this all needs to be done again with thoseXS
+         * */
         if(savedInstanceState == null){
 
 
             /*put whatever values you want in the bundle
-            * there is one bundle for each field name listed above*/
+             * there is one bundle for each field name listed above*/
+
+            /*
             Bundle[] bundles = new Bundle[fieldNames.length];
             for (int i = 0; i < bundles.length; i++){
                 bundles[i] = new Bundle();
@@ -76,38 +64,46 @@ public class TreeFormActivity extends AppCompatActivity {
                 bundles[i].putString("name", fieldNames[i]);
             }
 
+             */
+
             /*for every fragment you want to instantiate, add the line:
-            * .add(R.id.fragmentContainerId, FragmentClass.class, Bundle or null)
-            *
-            * Note: this line is not a full statement; without new lines what is below looks like this:
-            * getSupportFragmentManager().beginTransaction().setReorderingAllowed(true).add(R.id.field1, TextFieldFragment.class, bundles[0]).add(R.id.field2, TextFieldFragment.class, bundles[1]).commit();
-            * */
+             * .add(R.id.fragmentContainerId, FragmentClass.class, Bundle or null)
+             *
+             * Note: this line is not a full statement; without new lines what is below looks like this:
+             * getSupportFragmentManager().beginTransaction().setReorderingAllowed(true).add(R.id.field1, TextFieldFragment.class, bundles[0]).add(R.id.field2, TextFieldFragment.class, bundles[1]).commit();
+             * */
+
+            /*
             getSupportFragmentManager().beginTransaction()
                     .setReorderingAllowed(true)
-                    .add(R.id.field1, TextFieldFragment.class, bundles[0])
-                    .add(R.id.field2, TextFieldFragment.class, bundles[1])
+                    .add(R.id.tree_record_field1, TextFieldFragment.class, bundles[0])
+                    .add(R.id.tree_record_field2, TextFieldFragment.class, bundles[1])
+                    .add(R.id.tree_record_field3, TextFieldFragment.class, bundles[2])
+                    .add(R.id.tree_record_field4, TextFieldFragment.class, bundles[3])
+                    .add(R.id.tree_record_field5, TextFieldFragment.class, bundles[4])
+                    .add(R.id.tree_record_field6, TextFieldFragment.class, bundles[5])
+                    .add(R.id.tree_record_field7, TextFieldFragment.class, bundles[6])
+                    .add(R.id.tree_record_field8, TextFieldFragment.class, bundles[7])
+                    .add(R.id.tree_record_field9, TextFieldFragment.class, bundles[8])
+                    .add(R.id.tree_record_field10, TextFieldFragment.class, bundles[9])
+                    .add(R.id.tree_record_field11, TextFieldFragment.class, bundles[10])
+                    .add(R.id.tree_record_field12, TextFieldFragment.class, bundles[11])
+                    .add(R.id.tree_record_field13, TextFieldFragment.class, bundles[12])
+                    .add(R.id.tree_record_field14, TextFieldFragment.class, bundles[13])
+                    .add(R.id.tree_record_field15, TextFieldFragment.class, bundles[14])
+                    .add(R.id.tree_record_field16, TextFieldFragment.class, bundles[15])
+                    .add(R.id.tree_record_field17, TextFieldFragment.class, bundles[16])
+                    .add(R.id.tree_record_field18, TextFieldFragment.class, bundles[17])
+                    .add(R.id.tree_record_field19, TextFieldFragment.class, bundles[18])
+                    .add(R.id.tree_record_field20, TextFieldFragment.class, bundles[19])
                     .commit();
 
+             */
+
             /*there is information on receiving parameters in TextFieldFragment.java
-            * there is xml stuff that needs to be for each fragment as well, this is in tree_form.xml
-            * */
+             * there is xml stuff that needs to be for each fragment as well, this is in tree_form.xml
+             * */
         }
-
-        Button addNewTree = findViewById(R.id.AddNewTree);
-        addNewTree.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onButtonShowPopupWindowClick(view);
-            }
-        });
-
-        Button treeRecord = findViewById(R.id.tree_record1);
-        treeRecord.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(TreeFormActivity.this, TreeRecordActivity.class));
-            }
-        });
 
         Button backToForms = findViewById(R.id.backToForms);
         backToForms.setOnClickListener(new View.OnClickListener() {
@@ -121,7 +117,7 @@ public class TreeFormActivity extends AppCompatActivity {
         backToHome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(TreeFormActivity.this, MainActivity.class));
+                startActivity(new Intent(TreeRecordActivity.this, TreeFormActivity.class));
             }
         });
     }
